8b704773c28528772a18f9c67d0a38f6bd45db70
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-07-03 01:07:23 +0700

    .

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
   baseURL: "https://quizapi.hizkiajonathanbudiana.my.id",
   withCredentials: true,
 });
