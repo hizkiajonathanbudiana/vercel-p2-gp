4dca2d00901e6b80112a2b88f37f022f753a3099
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-07-17 17:14:22 +0700

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://quizapi.hizkiajonathanbudiana.my.id",
+  //baseURL: "https://quizapi.hizkiajonathanbudiana.my.id",
+  baseURL: "https://quizrushapi.hizkiajonathanbudiana.my.id",
   withCredentials: true,
 });
 
